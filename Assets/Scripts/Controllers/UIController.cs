58670af6d6ae90da1b632d476d7d11b0bb034bcf
Jadeskon <juergen.maier93@gmail.com>
2021-10-02 22:46:38 +0200

    UI Landmarker

--- Assets/Scripts/Controllers/UIController.cs
+++ Assets/Scripts/Controllers/UIController.cs
@@ -15,15 +15,23 @@ public class UIController : MonoBehaviour
     //public Image arrowRescuePlace;
 
     // Privates
-    private GameObject character;
+    private GameObject camera;
     //private GameObject rescuePlace;
     private GameObject vulcanPlace;
-    //private GameObject gameOverScreen;
+
+    private int score = 0;
+    private int passengers = 0;
+    private float time;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        character = GameObject.Find("Main Camera");
+        SetTimerDisplay(0);
+        SetScoreDisplay(0);
+        SetPasengersDisplay(0);
+
+        camera = GameObject.Find("Main Camera");
         //rescuePlace = GameObject.Find("House");
         vulcanPlace = GameObject.Find("Vulcan");
     }
@@ -31,42 +39,53 @@ public class UIController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 toPosition = vulcanPlace.transform.position;
-        Vector3 fromPosition = character.transform.position;
-
-        fromPosition.y = 0;
-        toPosition.y = 0;
-        //vulcanDirection.y = 0;
-
-        Vector3 dir = (toPosition - fromPosition).normalized;
-        
-
-        float angle = Vector3.Angle(fromPosition, toPosition);
-        arrowVulcan.transform.localEulerAngles = new Vector3(0, 0, angle);
-
-        /*Debug.Log(dir);
-        Debug.Log(angle);*/
-
-        /*
-        Vector3 rescuePlaceDirection = rescuePlace.transform.position - character.transform.position;
-        rescuePlaceDirection.y = 0;
-        rescuePlaceDirection.x = 180;
-        arrowRescuePlace.transform.rotation = Quaternion.Euler(rescuePlaceDirection);*/
-
-        textScoreComponent.text = "Score: " + 1;
-        textPassengerComponent.text = "Passengers: " + 2;
-        textTimeComponent.text = "Time: " + 8;// Time.realtimeSinceStartup;
-
-
-        //if (Input.GetKeyDown(KeyCode.W))
-        //    ActivateGameOverScreen(5, 8);
+        UpdateLandmarkIndicator(vulcanPlace, arrowVulcan);
     }
 
 
-    public void ActivateGameOverScreen(int score, int passengers)
+    public void ActivateGameOverScreen()
     {
-        textResults.text = "Score: " + score + "\nRescued Passengers: " + passengers;
-        gameOverScreen.SetActive(true);   
+        textResults.text = "Score: " + this.score + "\nRescued Passengers: " + this.passengers;
+        gameOverScreen.SetActive(true);
+    }
+
+    public void SetTimerDisplay(float time)
+    {
+        this.time = time;
+
+        int minutes = (int)(time / 60);
+        int seconds = ((int)time) % 60;
+
+        textTimeComponent.text = "Time: " + minutes.ToString("d2") + ":" + seconds.ToString("d2");
+    }
+
+    public void SetScoreDisplay(int score)
+    {
+        this.score = score;
+        textScoreComponent.text = "Score: " + score;
+    }
+
+    public void SetPasengersDisplay(int passengers)
+    {
+        this.passengers = passengers;
+        textPassengerComponent.text = "Passengers: " + passengers;
+    }
+
+    public void UpdateLandmarkIndicator(GameObject targetObject, Image image)
+    {
+        Vector3 toPosition = targetObject.transform.position;
+        Vector3 fromPosition = camera.transform.position;
+
+        Vector3 targetDirection = (fromPosition - toPosition).normalized;
+        
+        float radian = Mathf.Atan2(-fromPosition.x, fromPosition.z);
+        float degree = radian * 180 / Mathf.PI;
+
+        targetDirection.z = degree + camera.transform.rotation.eulerAngles.y;
+        targetDirection.x = 0;
+        targetDirection.y = 0;
+
+        image.transform.localRotation = Quaternion.Euler(targetDirection);
     }
 
     public void ExitButton()
