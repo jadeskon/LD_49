ea9340cc495b49a23c4424f7c1d9f02068d85296
Martin Gleich <martin.gleich3@web.de>
2021-10-03 10:58:03 +0200

    Scale distance to camera with speed.

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -1,4 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+
+public class RunningAverage
+{
+    int Count;
+    List<float> Values;
+    public RunningAverage(int count)
+    {
+        Count = count;
+        Values = new List<float>(count);
+    }
+    public float Push(float value)
+    {
+        if (Values.Count == Count)
+            Values.RemoveAt(0);
+        Values.Add(value);
+        return Average();
+    }
+    public void Clear()
+    {
+        Values.Clear();
+    }
+    public float Average() => Values.Average();
+}
 
 public class CameraController : MonoBehaviour
 {
@@ -6,12 +31,20 @@ public class CameraController : MonoBehaviour
     public GameObject vulcan; // The vulcan, or more generrall, the object thats always in view.
     public float Offset; // Distance of the camera behind the car.
     public float Height; // Dinstance of the camera above the car
+    private RunningAverage averageSpeed = new RunningAverage(30);
 
+	public static float Sigmoid(float value) {
+        return 1.0f / (1.0f + (float)System.Math.Exp(-value));
+	}
     public void LateUpdate()
     {
+        var characterSpeed = Vector3.Scale(character.GetComponent<Rigidbody>().velocity, new Vector3(1, 0.1f, 1)).magnitude;
+        var averagedSpeed = averageSpeed.Push(characterSpeed);
+        var scale = 1 + Sigmoid((averagedSpeed - 10) * 0.3f);
+
         var worldVulcanToCarVector = (character.transform.position - vulcan.transform.position).normalized;
         var worldCarToCamera = worldVulcanToCarVector * Offset + Vector3.up * Height;
-        transform.position = character.transform.position + worldCarToCamera;
+        transform.position = character.transform.position + scale * worldCarToCamera;
         transform.LookAt(character.transform);
     }
 }
